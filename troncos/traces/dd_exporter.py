190a9f6b37dcbfa905cc38eab43cbee5ed5895d4
RmStorm <roald.storm@oda.com>
2022-10-06 16:33:39 +0200

    Move opname and reource around

--- troncos/traces/dd_exporter.py
+++ troncos/traces/dd_exporter.py
@@ -12,16 +12,14 @@ from opentelemetry.sdk.util.instrumentation import (
 class OTLPSpanExporterDD(trace_exporter.OTLPSpanExporter):
     def export(self, spans: Sequence[ReadableSpan]) -> SpanExportResult:
         for sdk_span in spans:
-            if "dd_operation_name" in sdk_span.attributes:  # type: ignore[operator]
-                # I did not find a good way to get rid of the span kind so these
-                # dd_operation_name will almost always get '.internal' appended.
-                # I've been told it's not much of a problem.
-                sdk_span._instrumentation_scope = InstrumentationScope(
-                    name=sdk_span.attributes["dd_operation_name"],  # type: ignore # noqa: E501
-                    version="0.0.0",
-                )
-                sdk_span._instrumentation_info = InstrumentationInfo(
-                    name=sdk_span.attributes["dd_operation_name"],  # type: ignore # noqa: E501
-                    version="0.0.0",
-                )
+            sdk_span._instrumentation_scope = InstrumentationScope(
+                name=sdk_span.name,  # type: ignore # noqa: E501
+                version="0.0.0",
+            )
+            sdk_span._instrumentation_info = InstrumentationInfo(
+                name=sdk_span.name,  # type: ignore # noqa: E501
+                version="0.0.0",
+            )
+            if "resource" in sdk_span.attributes:
+                sdk_span._name = sdk_span.attributes["resource"]
         return super().export(spans)
