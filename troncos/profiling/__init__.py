9669d7ed4a72b705004ac5821ae7a0b8a578ba5e
Guðmundur Björn Birkisson <gudmundur.birkisson@oda.com>
2023-01-10 09:58:41 +0100

    Fix profile frame mapping

--- troncos/profiling/__init__.py
+++ troncos/profiling/__init__.py
@@ -28,12 +28,12 @@ def init_profiling_basic() -> Callable[[], Tuple[str, dict[str, str]]]:
         def _map_frames(events):  # type: ignore[no-untyped-def]
             for _, e1 in events.items():
                 for e2 in e1:
-                    new_frames = []
-                    for frame in e2.frames:
+                    e2.frames = list(e2.frames)
+                    for i in range(len(e2.frames)):
+                        frame = e2.frames[i]
                         file = frame[0].split("/lib/", 1)[-1]
                         loc = f"{file}:{frame[1]}:{frame[2]}"
-                        new_frames.append((frame[0], frame[1], loc, frame[3]))
-                    e2.frames = new_frames
+                        e2.frames[i] = (frame[0], frame[1], loc, frame[3])
 
         def export(self, events, start_time_ns, end_time_ns):  # type: ignore[no-untyped-def] # noqa: E501
             self._map_frames(events)  # type: ignore[no-untyped-call]
