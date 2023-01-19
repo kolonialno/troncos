11cea59d64623562399a396a47c7e06d6993bbd0
Guðmundur Björn Birkisson <gudmundur.birkisson@oda.com>
2023-01-19 13:16:12 +0100

    Make post_request_trace return trace_id

--- troncos/frameworks/gunicorn/__init__.py
+++ troncos/frameworks/gunicorn/__init__.py
@@ -1,5 +1,5 @@
 from troncos.traces.decorate import trace_block
-from troncos.traces.propagation import activate_context_from_dict
+from troncos.traces.propagation import activate_context_from_dict, get_trace_and_span_id
 
 SPAN_ATTR_NAME = "_troncos_gunicorn_trace_span"
 
@@ -19,8 +19,10 @@ def pre_request_trace(worker, req):  # type: ignore[no-untyped-def]
 
 
 def post_request_trace(worker, _req, _environ, res):  # type: ignore[no-untyped-def]
+    trace_id, _ = get_trace_and_span_id()
     span = getattr(worker, SPAN_ATTR_NAME, None)
     try:
         span.__exit__(None, None, None)  # type: ignore[union-attr]
     except:  # noqa: E722
         worker.log.exception("Exception finishing trace")
+    return None if trace_id is None else f"{trace_id:x}"
