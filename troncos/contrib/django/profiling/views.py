6244f8879796a70ac762844ac5b72ff08fa0e91c
Eirik Martiniussen Sylliaas <eirik@sylliaas.no>
2025-10-16 11:25:25 +0200

    Bump ddtrace and remove profiling support

--- troncos/contrib/django/profiling/views.py
+++
@@ -1,9 +0,0 @@
-from django.http import HttpRequest, HttpResponse
-
-from troncos.profiling.bootstrap.profile import get_profile
-
-
-def profiling_view(request: HttpRequest) -> HttpResponse:
-    profile, headers = get_profile()
-
-    return HttpResponse(profile, headers=headers)
--- troncos/profiling/__init__.py
+++
@@ -1,3 +0,0 @@
-from .pyroscope import start_py_spy_profiler
-
-__all__ = ["start_py_spy_profiler"]
